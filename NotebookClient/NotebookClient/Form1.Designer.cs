c1e4986175b3261dcc094c97f8a406e0bfb9d220
Dima <63181651+ZakharovDmitri@users.noreply.github.com>
2021-04-30 14:56:02 +0500

    Add files via upload

---
+++ NotebookClient/NotebookClient/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace NotebookClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView = new System.Windows.Forms.ListView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.clmFirstname = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.clmSecondname = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.clmBirthDay = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // listView
+            // 
+            this.listView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.clmFirstname,
+            this.clmSecondname,
+            this.clmBirthDay});
+            this.listView.FullRowSelect = true;
+            this.listView.GridLines = true;
+            this.listView.HideSelection = false;
+            this.listView.Location = new System.Drawing.Point(12, 12);
+            this.listView.Name = "listView";
+            this.listView.Size = new System.Drawing.Size(776, 295);
+            this.listView.TabIndex = 0;
+            this.listView.UseCompatibleStateImageBehavior = false;
+            this.listView.View = System.Windows.Forms.View.Details;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 349);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(158, 60);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Обновить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(215, 349);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(158, 60);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Добавить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(428, 349);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(158, 60);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Изменить";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(630, 349);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(158, 60);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Удалить";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // clmFirstname
+            // 
+            this.clmFirstname.Text = "Имя";
+            this.clmFirstname.Width = 258;
+            // 
+            // clmSecondname
+            // 
+            this.clmSecondname.Text = "Фамилия";
+            this.clmSecondname.Width = 258;
+            // 
+            // clmBirthDay
+            // 
+            this.clmBirthDay.Text = "Дата рождения";
+            this.clmBirthDay.Width = 258;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listView);
+            this.Name = "Form1";
+            this.Text = "Контакты";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ColumnHeader clmFirstname;
+        private System.Windows.Forms.ColumnHeader clmSecondname;
+        private System.Windows.Forms.ColumnHeader clmBirthDay;
+    }
+}
+
---
+++ NotebookClient/NotebookClient/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NotebookClient
+{
+    public partial class Form1 : Form
+    {
+        const string _baseAddress = "http://localhost:44321/";
+
+        private void Update()
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(_baseAddress);
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                HttpResponseMessage response;
+
+                response = client.GetAsync("api/People").Result;
+                if (response.IsSuccessStatusCode)
+                {
+                    Person[] reports = response.Content.ReadAsAsync<Person[]>().Result;
+                    foreach (var p in reports)
+                    {
+                        var item = new ListViewItem(new[] { p.Firstname, p.Secondname, p.BirthDay.ToShortDateString() });
+                        item.Tag = p.Id;
+                        listView.Items.Add(item);
+                    }
+                }
+            }
+        }
+
+        private void Delete(int delete)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:44321/");
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                HttpResponseMessage response = client.DeleteAsync("api/People/" + delete).Result;
+
+            }
+        }
+
+        private void Add()
+        {
+            Person newReport = new Person() { Firstname = "Коля", BirthDay = DateTime.Parse("01.02.2003") };
+
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(_baseAddress);
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                HttpResponseMessage response = client.PostAsJsonAsync("api/People", newReport).Result;
+
+            }
+        }
+
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (listView.SelectedItems.Count != 0)
+            {
+                int id = (int)listView.SelectedItems[0].Tag;
+
+                Delete(id);
+
+                Update();
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Update();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Add();
+        }
+    }
+}
